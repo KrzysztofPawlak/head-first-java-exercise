bfd57baa904f3e66e309396ee74281f1833ccc7c
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2017-12-23 12:46:52 +0100

    add chapter2

---
+++ src/ch2/DVDPlayer.java
@@ -0,0 +1,13 @@
+package ch2;
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+    void recordDVD() {
+        System.out.println("DVD recording");
+    }
+
+    void playDVD() {
+        System.out.println("Play DVD");
+    }
+}
---
+++ src/ch2/DrumKitTestDrive.java
@@ -0,0 +1,17 @@
+package ch2;
+
+public class DrumKitTestDrive {
+
+    public static void main(String [] args) {
+
+        DrumKit d = new DrumKit();
+        d.snare = false;
+        d.playSnare();
+
+        if (d.snare == true) {
+            d.playSnare();
+        }
+
+        d.playTopHat();
+    }
+}
