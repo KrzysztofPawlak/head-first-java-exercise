b932c156f0a5742e3496554212674059aa0ece93
KrzysztofPawlak <krzysztof.pawlak.tech@gmail.com>
2017-12-25 20:24:04 +0100

    add chapter4

---
+++ src/ch4/Clock.java
@@ -0,0 +1,14 @@
+package ch4;
+
+public class Clock {
+
+    String time;
+
+    void setTime(String t) {
+        time = t;
+    }
+
+    String getTime() {
+        return time;
+    }
+}
---
+++ src/ch4/ClockTestDrive.java
@@ -0,0 +1,11 @@
+package ch4;
+
+public class ClockTestDrive {
+    public static void main(String [] args) {
+        Clock c = new Clock();
+
+        c.setTime("1234");
+        String tod = c.getTime();
+        System.out.println("time: " + tod);
+    }
+}
